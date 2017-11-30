e3bbd64aa49c840351b07fe04167ffd7bab9e3a9
n-itotia <nigelnjagi@gmail.com>
2017-11-30 20:54:52 +0300

    assignment 1 java

---
+++ Assignment 1/src/assignment/pkg1/Assignment1.java
@@ -0,0 +1,21 @@
+/*
+ *Assignment 1
+ */
+package assignment.pkg1;
+
+/**
+ *
+ * @author rootlinux
+ */
+public class Assignment1 {
+
+    /**
+     * assignment 1
+     */
+    public static void main(String[] args) {
+        System.out.println("Nigel Njagi Itotia\nBBIT\nNigel.itotia@strathmore.edu");
+    System.out.print(2017-1998); System.out.println(" Yrs old");
+    
+    }
+    
+}
